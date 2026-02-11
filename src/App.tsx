1f95900bbd9856aa9c9a9f3efabb8e888cfa351e
tsteelematc <tsteele@madisoncollege.edu>
2026-02-11 09:45:02 -0600

    drawer...

--- src/App.tsx
+++ src/App.tsx
@@ -68,9 +68,28 @@ function App() {
       </div>
 
       <div className="card bg-base-100 w-full shadow-lg my-3">
-        <div className="card-body">
+        <div className="card-body p-2">
           <h2 className="card-title">Card title!</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+
+          <div className="drawer drawer-end">
+            <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              {/* Page content here */}
+              <label htmlFor="my-drawer-5" className="drawer-button btn btn-primary">Open drawer</label>
+            </div>
+            <div className="drawer-side">
+              <label htmlFor="my-drawer-5" aria-label="close sidebar" className="drawer-overlay"></label>
+              <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                {/* Sidebar content here */}
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+              </ul>
+            </div>
+          </div>
+
+
+
         </div>
       </div>
 
