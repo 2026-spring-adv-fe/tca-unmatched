811c9129794baf4ce754393ec84482042eaca6b9
tsteelematc <tsteele@madisoncollege.edu>
2026-02-11 12:43:03 -0600

    Added app padding...

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div
+      className='p-3'
+    >
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -35,7 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
