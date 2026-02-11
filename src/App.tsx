dd7b340d66be0b5b595db58dcb9c30b3d7f93083
tsteelematc <tsteele@madisoncollege.edu>
2026-02-11 09:42:41 -0600

    Collapse on top of timeline ? ? ?

--- src/App.tsx
+++ src/App.tsx
@@ -78,6 +78,13 @@ function App() {
         <div className="card-body p-2">
           <h2 className="card-title">Card title!</h2>
 
+          <div tabIndex={0} className="collapse collapse-arrow bg-base-100 border-base-300 border">
+            <div className="collapse-title font-semibold">How do I create an account?</div>
+            <div className="collapse-content text-sm">
+              Click the "Sign Up" button in the top right corner and follow the registration process.
+            </div>
+          </div>
+
           <ul className="timeline timeline-vertical">
             <li>
               <div className="timeline-middle">
