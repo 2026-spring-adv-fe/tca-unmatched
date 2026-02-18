cb0d18725d6226d74a3cf2aa6cc7fb3eac1553f3
tsteelematc <tsteele@madisoncollege.edu>
2026-02-18 13:41:34 -0600

    Got routing working...
    Next lets put some buttons in those components...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import './App.css'
+import { 
+  HashRouter,
+  Routes,
+  Route, 
+} from 'react-router';
+
+const Home = () => <h1>Home</h1>;
+const Setup = () => <h1>Setup</h1>;
+const Play = () => <h1>Play</h1>;
 
 const App = () => {
 
   return (
     <div>
-      Cleared everything out...
+      <HashRouter>
+        <Routes>
+          <Route 
+            path='/'
+            element={
+              <Home />
+            }
+          />
+          <Route 
+            path='/setup'
+            element={
+              <Setup />
+            }
+          />
+          <Route 
+            path='/play'
+            element={
+              <Play />
+            }
+          />          
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
