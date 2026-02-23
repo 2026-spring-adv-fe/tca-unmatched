9576ec92c5cf72a9f1594fef61910f9fc5efc896
tsteelematc <tsteele@madisoncollege.edu>
2026-02-23 13:08:59 -0600

    Harvested from TS Playground...

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,31 @@ import {
 import { Home } from './Home';
 import { Setup } from './Setup';
 import { Play } from './Play';
+import type { GameResult } from './GameResults';
+
+
+const dummyGameResults: GameResult[] = [
+    {
+        winner: "Harry",
+        players: [
+            "Harry",
+            "Hermione",
+            "Ron",
+        ],
+        start: "2026-02-01T18:53:59.078Z",
+        end: "2026-02-01T19:27:59.078Z",
+    },
+    {
+        winner: "Hermione",
+        players: [
+            "Harry",
+            "Hermione",
+            "Ron",
+        ],
+        start: "2026-01-15T22:07:59.078Z",
+        end: "2026-01-15T23:01:59.078Z",
+    },  
+];
 
 const App = () => {
 
---
+++ src/GameResults.ts
@@ -0,0 +1,61 @@
+//
+// Exported type definitions...
+//
+export type GameResult = {
+    winner: string;
+    players: string[];
+
+    start: string;
+    end: string;
+};
+
+export type GeneralFacts = {
+    lastPlayed: string;
+    totalGames: number;
+    shortestGame: string;
+    longestGame: string;
+};
+
+//
+// Exported funcs...
+//
+export const getGeneralFacts = (games: GameResult[]): GeneralFacts => {
+
+    if (games.length === 0) {
+        return {
+            lastPlayed: "N/A",
+            totalGames: 0,
+            shortestGame: "N/A",
+            longestGame: "N/A",
+        };
+    }
+
+    const now = Date.now();
+
+    const gamesLastPlayedAgoInMilliseconds = games.map(
+        x => now - Date.parse(x.end)
+    );
+
+    const mostRecentlyPlayedInMilliseconds = Math.min(
+        ...gamesLastPlayedAgoInMilliseconds
+    );
+
+    const gameDurationsInMilliseconds = games.map(
+        x => Date.parse(x.end) - Date.parse(x.start)
+    );
+
+    // console.log(
+    //     gamesLastPlayedAgoInMilliseconds
+    // );
+
+    return {
+        lastPlayed: `${mostRecentlyPlayedInMilliseconds / 1000 / 60 / 60/ 24} days ago`,
+        totalGames: games.length,
+        shortestGame: `${Math.min(...gameDurationsInMilliseconds) / 1000 / 60} minutes`,
+        longestGame: `${Math.max(...gameDurationsInMilliseconds) / 1000 / 60} minutes`,
+    };
+};
+
+//
+// Helper funcs...
+//
