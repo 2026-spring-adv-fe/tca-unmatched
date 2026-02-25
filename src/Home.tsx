b2f6ee55fe57b0b1b1687bd9590ba5117b58b033
tsteelematc <tsteele@madisoncollege.edu>
2026-02-25 13:50:03 -0600

    Daisy UI cards with fun facts

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,6 +37,39 @@ export const Home: React.FC<HomeProps> = ({
             >
                 Setup a Game
             </button>
+
+            <div className="card bg-base-100 w-full shadow-lg my-5">
+                <div className="card-body p-2">
+                    <h2 className="card-title">General Facts</h2>
+                    <table className="table table-zebra">
+                        <tbody>
+                            <tr>
+                                <td>Last Played</td>
+                                <th>{generalFacts.lastPlayed}</th>
+                            </tr>
+                            <tr>
+                                <td>Total Games</td>
+                                <th>{generalFacts.totalGames}</th>
+                            </tr>
+                            <tr>
+                                <td>Shortest Game</td>
+                                <th>{generalFacts.shortestGame}</th>
+                            </tr>
+                            <tr>
+                                <td>Longest Game</td>
+                                <th>{generalFacts.longestGame}</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 w-full shadow-lg my-5">
+                <div className="card-body p-2">
+                    <h2 className="card-title">Most Popular First Move</h2>
+                    <p>{popularMove}</p>
+                </div>
+            </div>
         </>
     );
 };
