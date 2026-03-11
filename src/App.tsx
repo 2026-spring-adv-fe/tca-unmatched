1f87d8fe9e21385918e803f04a3f41a7c33ccb66
tsteelematc <tsteele@madisoncollege.edu>
2026-03-11 13:12:28 -0500

    half way there, saving chosen theme with localforage

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
   type GameResult, 
 } from './GameResults';
 import { useState } from 'react';
+import localforage from 'localforage';
 
 
 const dummyGameResults: GameResult[] = [
@@ -85,11 +86,18 @@ const App = () => {
           <input 
             type="checkbox" 
             onClick={
-              () => setTheme(
-                theme === "light"
-                  ? "dark"
-                  : "light"
-              )
+              async () => {
+                const result = await localforage.setItem<string>(
+                  'theme',
+                  theme === "light"
+                    ? "dark"
+                    : "light",
+                );
+
+                setTheme(
+                  result
+                );
+              }
             }
           />
 
