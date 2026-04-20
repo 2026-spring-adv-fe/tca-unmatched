f6fb97446c7015155ca6958f210943887adb96e9
tsteelematc <tsteele@madisoncollege.edu>
2026-04-20 11:17:28 -0500

    dialog with email input and state
    next localforage

--- src/App.tsx
+++ src/App.tsx
@@ -336,6 +336,7 @@ const App = () => {
   // const currentPlayersStateTuple = useState<Player[]>([]);
 
   const emailDialog = useRef<HTMLDialogElement | null>(null);
+  const [emailInDialog, setEmailInDialog] = useState("");
 
   useEffect(
     () => {
@@ -506,12 +507,23 @@ const App = () => {
           className="modal modal-bottom sm:modal-middle"
         >
           <div className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click the button below to close</p>
+            <h3 className="font-bold text-lg">Email for loading/saving game results...</h3>
+
+            <input 
+                type="text"
+                className='input input-bordered w-full mt-3'
+                placeholder="Email address" 
+                value={emailInDialog}
+                onChange={
+                    (e) => setEmailInDialog(
+                        e.target.value
+                    )
+                }
+            />            
             <div className="modal-action">
               <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
-                <button className="btn">Close</button>
+                <button className="btn">Save</button>
               </form>
             </div>
           </div>
